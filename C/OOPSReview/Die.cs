e2c012865f94343d5b1a8d4edcca1c14e40d8fe2
Xanderman02 <45209830+Xanderman02@users.noreply.github.com>
2019-01-17 16:44:05 -0700

    Programmed the main
    and the turn class

--- C/OOPSReview/Die.cs
+++ C/OOPSReview/Die.cs
@@ -120,7 +120,7 @@ namespace OOPSReview
         //   pass in a set of values which will be used at the 
         //   time of instance creation to set the values of the 
         //   internal data members/auto properties
-        public Die(int sides, string colour, int faceValue)
+        public Die(int sides, string colour)
         {
             Sides = sides;
             Colour = colour;
---
+++ C/OOPSReview/Turn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSReview
+{
+    public class Turn
+    {
+        // auto implimented properties
+        public int Player1 { get; set; }
+        public int Player2 { get; set; }
+
+        // the next segment is optional
+
+        public Turn()
+        {
+            
+        }
+
+        public Turn(int player1, int player2)
+        {
+            Player1 = player1;
+            Player2 = player2;
+        }
+
+    }
+}
