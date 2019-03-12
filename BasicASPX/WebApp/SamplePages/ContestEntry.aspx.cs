50333a3d5d690e9a1698ace41a6546c8121d7250
Xanderman02 <45209830+Xanderman02@users.noreply.github.com>
2019-03-12 15:57:56 -0600

    setup for data collection

--- BasicASPX/WebApp/SamplePages/ContestEntry.aspx.cs
+++ BasicASPX/WebApp/SamplePages/ContestEntry.aspx.cs
@@ -8,10 +8,40 @@ using System.Web.UI.WebControls;
 namespace WebApp.SamplePages
 {
     public partial class ContestEntry : System.Web.UI.Page
-    {
+    {   // if we had a database the data would be stored there
+        // using this static List<T> is ONLY done in this example because we have no database
+        public static List<EntryCollection> ContestEntryCollection;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             Message.Text = "";
+            // test Page.IsPostBack to page initialization
+            if(!Page.IsPostBack)
+            {
+                ContestEntryCollection = new List<EntryCollection>();
+            }
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            // validate the data coming in
+            // validate the user checking the terms
+            //      yes: create/load entry, add to List, display List
+            //       no: message
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            FirstName.Text = "";
+            LastName.Text = "";
+            StreetAddress1.Text = "";
+            StreetAddress2.Text = "";
+            City.Text = "";
+            Province.SelectedIndex = 0;
+            PostalCode.Text = "";
+            EmailAddress.Text = "";
+            CheckAnswer.Text = "";
+            Terms.Checked = false;
         }
     }
 }
